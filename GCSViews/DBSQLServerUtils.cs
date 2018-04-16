4c4ee011604e926781357b3eb8883f7943218f9c
Napat <paper-pat@hotmail.com>
2018-04-17 01:48:32 +0700

    Run on Napat PC

--- GCSViews/DBSQLServerUtils.cs
+++ GCSViews/DBSQLServerUtils.cs
@@ -14,9 +14,6 @@ namespace Tutorial.SqlConn
         public static SqlConnection
                  GetDBConnection(string datasource, string database, string username, string password)
         {
-            //
-            // Data Source=TRAN-VMWARE\SQLEXPRESS;Initial Catalog=simplehr;Persist Security Info=True;User ID=sa;Password=12345
-            //
             string connString = @"Data Source=" + datasource + ";Initial Catalog="
                         + database + ";Integrated Security=True";
 
--- GCSViews/DBUtils.cs
+++ GCSViews/DBUtils.cs
@@ -11,11 +11,11 @@ namespace Tutorial.SqlConn
     {
         public static SqlConnection GetDBConnection()
         {
-            string datasource = @"CS-RABBIT\SQLEXPRESS";
+            string datasource = @"napat";
 
             string database = "DroneFlightPlanner";
-            string username = "test";
-            string password = "P@ssw0rd";
+            string username = "";
+            string password = "";
 
             return DBSQLServerUtils.GetDBConnection(datasource, database, username, password);
         }
